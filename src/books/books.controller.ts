f3fc7630b04ea043db37ca865ddf7c85f0b279d6
unknown <ovcharenko.olga.2014@gmail.com>
2024-03-02 22:07:00 +0300

    change books.services and controller by adding endpoints

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,7 +1,53 @@
 import { Controller } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Get, Delete, Put, Post, Body, Param } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { CreateBookDTO } from './dtos/create-book.dto';
+import { UpdateBookDTO } from './dtos/update-book.dto';
 
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService ) {}
+
+  @Get('/')
+  async getAll() {
+    return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id') id: string) {
+    const order = await this.booksService.getById(id);
+    if (!order) throw new NotFoundException('Product not found');
+    return order;
+  }
+
+
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+
+    if (!(await this.booksService.getById(id)))
+      throw new NotFoundException('Order not found');
+
+    await this.booksService.deleteById(id);
+    return { success: true };
+  }
+
+  @Post('/')
+    create(@Body() orderData: CreateBookDTO) {
+    return this.booksService.create(orderData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() orderData: UpdateBookDTO,
+  ){
+    if (!(await this.booksService.getById(id)))
+      throw new NotFoundException('Order not found');
+
+    await this.booksService.updateById(id, orderData);
+    return { success: true };
+  }
+  
 }
---
+++ src/books/dtos/update-book.dto.ts
@@ -0,0 +1,30 @@
+import {
+    IsNotEmpty,
+    IsString,
+    Length,
+    IsInt,
+    Min,
+    IsUUID
+} from 'class-validator';
+  
+export class UpdateBookDTO {
+    @IsNotEmpty()
+    @IsString()
+    @Length(10, 20)
+    title: string;
+  
+    @IsNotEmpty()
+    @IsInt()
+    @Min(0)
+    rating: number;
+
+    @IsNotEmpty()
+    @IsInt()
+    @Min(0)
+    price: number;
+
+    @IsNotEmpty()
+    @IsUUID()
+    @IsString()
+    authorId: string;
+}
